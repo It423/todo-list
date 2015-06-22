37cd82d7762ddf9d901873c5339dd72c9c24bb27
It423 <it42@hotmail.co.uk>
2015-06-22 19:05:39 +0100

    Add XML documentation

--- Todo-List/Todo-List/Program.cs
+++ Todo-List/Todo-List/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Program.cs
+// <copyright file="Program.cs"> This code is protected under the MIT License. </copyright>
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,16 @@ using System.Threading.Tasks;
 
 namespace Todo_List
 {
-    class Program
+    /// <summary>
+    /// An application entry point for a console-based hand of Whist.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the program.
+        /// </summary>
+        /// <param name="args"> Any arguments the program is run with. </param>
+        public static void Main(string[] args)
         {
         }
     }
