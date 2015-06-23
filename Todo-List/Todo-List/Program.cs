c8b71ecdbc8ad0e1ea6d90a0b660ce7a9a429931
It423 <it42@hotmail.co.uk>
2015-06-23 19:42:27 +0100

    Add a help function
    Add a function to display help information about the program.

--- Todo-List/Todo-List/Program.cs
+++ Todo-List/Todo-List/Program.cs
@@ -19,6 +19,23 @@ namespace Todo_List
         /// <param name="args"> Any arguments the program is run with. </param>
         public static void Main(string[] args)
         {
+            Help();
+            Console.ReadKey();
         }
+
+        /// <summary>
+        /// Displays help information about the program.
+        /// </summary>
+        public static void Help()
+        {
+            Console.WriteLine("Welcome to George Wright's todo list program!");
+            Console.WriteLine("To add a note use:\n\t/a [Name] [Category1, Catergory2...]\n");
+            Console.WriteLine("To view notes use:\n\t/v [Category1, Catergory2...]\n");
+            Console.WriteLine("To remove a note use:\n\t/r [Name]\n");
+            Console.WriteLine("To edit a note use:\n\t/e [Name] [New name] [New Category1, New Catergory2...]\n");
+            Console.WriteLine("Categories are not allways needed in the command. When adding/editing a note, it will add to the uncategorised list.");
+            Console.WriteLine("When viewing a note it will display all categories.\n");
+            Console.WriteLine("To display this help again use:\n\t/h\n\n");        
+        } 
     }
 }
