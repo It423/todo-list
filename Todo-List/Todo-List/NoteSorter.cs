c2fe56838b15a251a6ea69b9fd646fa1003072e8
It423 <it42@hotmail.co.uk>
2015-06-23 19:14:32 +0100

    Add more methods to sort by
    Added more methods to sort the notes by including:
    - Add note
    - Remove notes
    - Edit notes
    - Get note index

--- Todo-List/Todo-List/NoteSorter.cs
+++ Todo-List/Todo-List/NoteSorter.cs
@@ -34,10 +34,51 @@ namespace Todo_List
             IEnumerable<Note> notesUnderCategories = Notes;
             foreach (string catergory in categoryNames)
             {
-                notesUnderCategories = notesUnderCategories.Where(n => n.Categories.Contains(catergory.ToUpper()));
+                notesUnderCategories = notesUnderCategories.Where(n => n.Categories.Select(c => c = c.ToUpper()).Contains(catergory.ToUpper()));
             }
 
             return notesUnderCategories.ToList();
+        }
+
+        /// <summary>
+        /// Adds a note to the collection of notes.
+        /// </summary>
+        /// <param name="note"> The note to add. </param>
+        public static void AddNote(Note note)
+        {
+            Notes.Add(note);
+        }
+
+        /// <summary>
+        /// Removes a note from the collection of notes.
+        /// </summary>
+        /// <param name="note"> The note to remove. </param>
+        public static void RemoveNote(Note note)
+        {
+            Notes.Remove(note);
+        }
+
+        /// <summary>
+        /// Edits a note.
+        /// </summary>
+        /// <param name="oldNote"> The old note to edit. </param>
+        /// <param name="newNote"> The new note to replace the old. </param>
+        public static void EditNote(Note oldNote, Note newNote)
+        {
+            int i = Notes.IndexOf(oldNote);
+            Notes[i] = newNote;
+        }
+
+        /// <summary>
+        /// Finds the index of a note by the title.
+        /// </summary>
+        /// <param name="title"> The title of the note. </param>
+        /// <returns> The index of the note in the list. </returns>
+        public static int NoteIndex(string title)
+        {
+            // Get the note by the title
+            Note note = Notes.Where(n => n.Title == title).ToArray()[0];
+            return Notes.IndexOf(note);
         }
     }
 }
