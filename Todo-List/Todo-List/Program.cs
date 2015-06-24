c836c7b0443c8357f7e18d642319ac2ba96271d4
It423 <it42@hotmail.co.uk>
2015-06-24 07:56:43 +0100

    Implement remove function
    Wrtie the function to remove a note. Also add the command /h to display
    help.

--- Todo-List/Todo-List/Program.cs
+++ Todo-List/Todo-List/Program.cs
@@ -43,6 +43,9 @@ namespace Todo_List
                     case "/c":
                         EditCategory(cmd);
                         break;
+                    case "/h":
+                        Help();
+                        break;
                     case "/q":
                         exit = true;
                         break;
@@ -64,15 +67,15 @@ namespace Todo_List
         public static void Help()
         {
             Console.WriteLine("Welcome to George Wright's todo list program!");
-            Console.WriteLine("To add a note use:\n\t/a [\"Note\"] [Category1, Catergory2...]\n");
-            Console.WriteLine("To view notes use:\n\t/v [Category1, Catergory2...]\n");
-            Console.WriteLine("To remove a note use:\n\t/r [\"Note\"]\n");
-            Console.WriteLine("To edit a note use:\n\t/e [\"Note\"] [\"New Note\"]\n");
-            Console.WriteLine("To edit a note's categories:\n\t/c [\"Note\"] [Category1, Catergory2...]\n");
-            Console.WriteLine("Categories are not allways needed in the command. When addinga note or editing its categories, it will add to the uncategorised list.");
+            Console.WriteLine("To add a note use:\n\t/a \"Note\" Category1;Catergory2...\n");
+            Console.WriteLine("To view notes use:\n\t/v Category1;Catergory2...\n");
+            Console.WriteLine("To remove a note use:\n\t/r \"Note\"\n");
+            Console.WriteLine("To edit a note use:\n\t/e \"Note\" \"New Note\"\n");
+            Console.WriteLine("To edit a note's categories:\n\t/c \"Note\" Category1;Catergory2...\n");
+            Console.WriteLine("Categories are not allways needed in the command. When adding a note or editing it's categories, it will add to the uncategorised list if nothing is entered.");
             Console.WriteLine("When viewing a note it will display all categories.\n");
             Console.WriteLine("To exit the program, you can close it via the cross or enter:\n\t/q\n");
-            Console.WriteLine("To display this help again use:\n\t/h\n\n");        
+            Console.WriteLine("To display this help again use:\t/h\n");        
         } 
 
         /// <summary>
@@ -111,7 +114,7 @@ namespace Todo_List
                 }
 
                 // Construct the list of categories
-                string[] categories = catString.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+                string[] categories = catString.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
 
                 // Create and add the note
                 NoteSorter.AddNote(new Note(name, categories));
@@ -124,7 +127,7 @@ namespace Todo_List
         /// <param name="cmd"> The command inputted. </param>
         public static void View(string cmd)
         {
-
+            
         }
 
         /// <summary>
@@ -133,7 +136,26 @@ namespace Todo_List
         /// <param name="cmd"> The command inputted. </param>
         public static void Remove(string cmd)
         {
+            // Get note name
+            string name;
+            try
+            {
+                name = cmd.Split(new char[] { '"' }, StringSplitOptions.RemoveEmptyEntries)[1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                name = string.Empty;
+            }
 
+            // Throw error if name doesn't exists or no name was provided
+            if (NoteSorter.NoteIndex(name) == -1 || name.Length == 0)
+            {
+                Console.WriteLine("Error! No name was provided, or the note doesn't exists!");
+            }
+            else
+            {
+                NoteSorter.RemoveNote(NoteSorter.Notes[NoteSorter.NoteIndex(name)]);
+            }
         }
 
         /// <summary>
